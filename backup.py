ea3efc8d4402699e0fe8eb243bb23c0cb2c65128
jaykurgat <87975485+jaykurgat@users.noreply.github.com>
2024-01-24 14:48:58 +0300

    Add files via upload

---
+++ backup.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+import datetime
+import time
+import schedule
+
+source_dir = "C:/Users/James Kurgat/Desktop/applications"
+destination_dir = "C:/Users/James Kurgat/Desktop/Backups"
